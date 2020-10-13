95224950ce8120b074f13f48e6e62fa9615387bc
Han Min Htet <mghanmhtet91@gmail.com>
2020-10-13 21:37:17 +0630

    php lesson

--- assignment3.php
+++ assignment3.php
@@ -7,10 +7,16 @@
         'Philadelphia, PA' =>'1,526,006',
         'Phoenix, AZ' =>'1,445,632',
         'San Antonio, TX' =>'1,327,407',
-        'San Diego, CA' =>'	1,307,402',
+        'San Diego, CA' =>'1,307,402',
         'Dallas, TX' =>'1,197,816',
         'San Jose, CA' =>'945,942',
         );
+    $population = array(8175133,3792621,2695598,2100263,1526006,1445632,1327407,1307402,1197816,945942);
+    $res = 0;
+    foreach($population as $value){
+        $res += $value;
+    }
+    $result = number_format($res);
     
     echo "<h2>Total population  in all 10 cities</h2>";
     echo "<table border=1 cellpadding=10px>";
@@ -18,6 +24,7 @@
     foreach ($city as $key => $value) {
         echo "<tr><td>$key</td><td>$value</td></tr>";
     }
+    echo "<tr><td><b>Total population</b></td><td><b>$result</b></td></tr>";
     echo "</table>";
     echo "<br><hr>";
 
@@ -35,8 +42,8 @@
     echo "<h2>Order by City name</h2>";
     echo "<table border=1 cellpadding=10px>";
     echo "<tr><th>City</th><th>Population</th>";
-    foreach ($city  as $pkey => $pvalue) {
-        echo "<tr><td>$pkey</td><td>$pvalue</td></tr>";
+    foreach ($city  as $key => $value) {
+        echo "<tr><td>$key</td><td>$value</td></tr>";
     }
     echo "</table>";
 ?>
